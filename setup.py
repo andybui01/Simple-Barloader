89d387edf58b9012c026e8bd5a54cd7ae130ca46
andybui01 <andy.bui2001@gmail.com>
2020-02-25 08:35:13 +1100

    rename to simple-barloader

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='Simple-Barloader',  
-     version='1.0',
-     scripts=['Simple-Barloader'] ,
+     name='simple_barloader',  
+     version='1.0.1',
+     scripts=['simple_barloader'] ,
      author="Andy Bui",
      author_email="andy.bui2001@gmail.com",
      description="Bar loading graphic generator",
      long_description=long_description,
    long_description_content_type="text/markdown",
-     url="https://github.com/andybui01/Simple-Barloader/",
+     url="https://github.com/andybui01/simple_barloader/",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
